c3cd7b370a75a66f863e6272ed89321d24c1da7f
Diego Neri <diego@diegoneri.com>
2023-11-30 23:41:59 +0000

    Cria container para runtime

--- todo/src/main/java/com/fatecbs/ToDo/config/SwaggerConfig.java
+++ todo/src/main/java/com/fatecbs/ToDo/config/SwaggerConfig.java
@@ -3,15 +3,19 @@ package com.fatecbs.ToDo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://fictional-space-goggles-rpq5wq9r6r2pg7p-8080.app.github.dev", description = "Default Server URL")})
 @Configuration
 public class SwaggerConfig {
   @Bean
   public OpenAPI financasOpenAPI() {
-     return new OpenAPI().info(new Info()
+     return new OpenAPI()
+      .info(new Info()   
         .title("API de gerenciamento de tarefas")
         .description("Esta API é utilizada na disciplina Desenvolvimento para Servidores-II")
         .version("v0.0.1")
